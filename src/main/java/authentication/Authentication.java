766ef92969aacfd93630fa471a2007f43379caa2
rubendjg <rdejuangrande@gmail.com>
2025-11-24 19:13:17 +0100

    ADD: Added the authentication folder and class, with the login and signup methods. Created User class

---
+++ src/main/java/authentication/Authentication.java
@@ -0,0 +1,30 @@
+package authentication;
+public class Authentication {
+    public static boolean logIn(String email, String password) {
+        // TODO: Decide method to store data
+        String auxPassword = "example password";
+        if (password.equals(auxPassword)) {
+            return true;
+        }
+        return false;
+    }
+
+    // Returns true if the user could be created and false if not
+    public static boolean singUp(String email, String firstName, String lastName, String password){
+        if(checkUniqueEmail(email)){
+            User newUser = new User(email, firstName, lastName, password);
+            return true;
+        }
+        return false;
+    }
+
+    // Checks if the email already exists in our database
+    private static boolean checkUniqueEmail(String email){
+        // TODO: Decide method to store data
+        String auxEmail = "example email";
+        if (email.equals(auxEmail)) {
+            return true;
+        }
+        return false;
+    }
+}
