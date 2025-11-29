766fe111a5f6d5c21974507dc240dd24a92bdd6c
Daniel <daguilar.ieu2023@student.ie.edu>
2025-11-29 23:17:01 +0100

    ADD: TODO's for doctors, and made USER abstract

--- src/main/java/Users/Doctor.java
+++ src/main/java/Users/Doctor.java
@@ -5,19 +5,29 @@ import Appointments.Appointment;
 import java.util.Date;
 
 public class Doctor extends User {
-    public Doctor(int id, String email, String firstName, String lastName, String password) {
-        super(id, email, firstName, lastName, password);
+    public Doctor(int id, String firstName, String lastName, String email, String password) {
+        super(id, firstName, lastName, email, password);
     }
 
-    public Appointment createAppointment(Date date){
-        // TODO: Check if the date is valid
-        Appointment newAppointment = new Appointment(0, this, null, date);
-        // TODO: Store it in the database
-        return newAppointment;
+    // TODO: Allow doctor to create a schedule slot (availability)
+    public void createScheduleSlot(LocalDateTime start, LocalDateTime end) {
+        // Implementation planned later
     }
 
-    public void addAppointment(Date date){
-        Appointment newAppointment = createAppointment(date);
-        this.appointments.add(newAppointment);
+    // TODO: Allow doctor to create a schedule slot (availability)
+    public void createScheduleSlot(LocalDateTime start, LocalDateTime end) {
+        // Implementation planned later
     }
+
+    // TODO: Allow doctor to view all their appointments
+    public List<Appointment> getAppointments() {
+        // Fetch from database by doctorId
+        return null;
+    }
+
+
+
+
+
+
 }
--- src/main/java/Users/Patient.java
+++ src/main/java/Users/Patient.java
@@ -7,13 +7,13 @@ import static DatabaseManagement.AppointmentService.fetchAppointment;
 
 public class Patient extends User {
     // Constructor
-    public Patient(int id, String email, String firstName, String lastName, String password) {
-        super(id, email, firstName, lastName, password);
+    public Patient(int id, String firstName, String lastName, String email, String password) {
+        super(id, firstName, lastName, email, password);
     }
 
     public void addAppointment(Date date){
         Date now = new Date();
-        Appointment newAppointment = fetchAppointment(1, now); // TODO: How do we want to make users select a doctor?
+        Appointment newAppointment = fetchAppointment(1, now); // TODO: How do we want to make users select a doctor? good question ruben
         newAppointment.setPatient(this);
         // TODO: Update it in the database
         this.appointments.add(newAppointment);
