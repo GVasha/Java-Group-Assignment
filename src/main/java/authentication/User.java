bf73f756db2f91abb6c1ba7b72fe7ffb028c921d
Daniel <daguilar.ieu2023@student.ie.edu>
2025-11-29 16:34:32 +0100

    ADD: setters/getters for USER aswell as modify User constructor

--- src/main/java/authentication/User.java
+++ src/main/java/authentication/User.java
@@ -6,10 +6,53 @@ public class User {
     private String firstName;
     private String lastName;
     private String password;
+    private String location;
+    private Role role;
 
-    public User(String email, String firstName, String lastName, String password){
+    protected User(String email, String firstName, String lastName, String password, String location, Role role) {
         this.email = email;
         this.firstName = firstName;
         this.password = password;
+        this.lastName = lastName;
+        this.location = location;
+        this.role = role;
     }
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+
 }
