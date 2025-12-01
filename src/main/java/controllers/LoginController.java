50518d24cca38863f3d429c5103fd68b3e20e290
BigosKAR <oskarkloczko13@gmail.com>
2025-12-01 14:39:45 +0100

    connects java fx create account button to the login controller and accesses the text fields

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController extends BaseController {
+
+    @FXML
+    private TextField usernameSignUp;
+
+    @FXML
+    private TextField emailSignUp;
+
+    @FXML
+    private PasswordField passwordSignUp;
+
+    @FXML
+    public void handleSignUpSubmit(){
+        System.out.println("PRESSED BUTTON!");
+        String username = usernameSignUp.getText();
+        String email = emailSignUp.getText();
+        String password = passwordSignUp.getText();
+        System.out.println(username + " " + email + " " + password);
+    }
 }
